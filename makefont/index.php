5e254c7e71fe79932187b8d89c1ff43c84747150
stekot <stepan.kotek@gmail.com>
2012-04-26 12:24:46 +0200

    Example and documentation updated.

--- makefont/index.php
+++ makefont/index.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'makefont.php';
-$font = 'ubuntu';
-$enc = 'cp1250';
-$embed = true;
+$font = 'ubuntu.ttf'; //path to font file
+$enc = 'cp1250'; // encoding used in font - default: cp1252
+$embed = true; // font is embeded to pdf file
 makefont($font.'.ttf', $enc, $embed);
 echo "<br>".file_get_contents($font.".js");
